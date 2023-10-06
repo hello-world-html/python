a42379177142fed0ad3f85817b2a07565a9080d3
hi <138982320+hello-world-html@users.noreply.github.com>
2023-10-06 14:00:09 +0800

    6

---
+++ game/test/block.py
@@ -0,0 +1,20 @@
+class Block(pygame.sprite.Sprite):
+    def __init__(self, blockType, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(BLOCK_RES[blockType])
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+    def update(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        elif pressed[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+        elif pressed[K_UP]:
+            self.rect.move_ip(0, -1)
+        elif pressed[K_DOWN]:
+            self.rect.move_ip(0, 1)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
--- game/test/main.py
+++ game/test/main.py
@@ -73,26 +73,6 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 DISPLAY = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-class Block(pygame.sprite.Sprite):
-    def __init__(self, blockType, pos):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(BLOCK_RES[blockType])
-        self.rect = self.image.get_rect()
-        self.rect.center = pos
-
-    def update(self):
-        pressed = pygame.key.get_pressed()
-        if pressed[K_LEFT]:
-            self.rect.move_ip(-1, 0)
-        elif pressed[K_RIGHT]:
-            self.rect.move_ip(1, 0)
-        elif pressed[K_UP]:
-            self.rect.move_ip(0, -1)
-        elif pressed[K_DOWN]:
-            self.rect.move_ip(0, 1)
-
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
 
 P = Block(BlockType.PURPLE, (200, 300))
 C = Block(BlockType.CYAN, (600, 300))
