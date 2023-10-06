f93005739544f8b216103710a3505c734f958ed7
hi <138982320+hello-world-html@users.noreply.github.com>
2023-10-06 13:53:05 +0800

    3

--- test/test.py
+++ test/test.py
@@ -1,9 +1,13 @@
-from typing import Any
-import pygame , sys
+import pygame
+import sys
 from pygame.locals import *
 import random
+
 pygame.init()
-DIsplay = pygame.display.set_mode((800,600))
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+DISPLAY = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 class BlockType:
     RED = 0
     GREEN = 1
@@ -13,34 +17,114 @@ class BlockType:
     PURPLE = 5
     CYAN = 6
     BLOCKMAX = 7
-Block_RES = {
-    BlockType.RED:"pic/red.png"
-    }
+
+BLOCK_RES = {
+    BlockType.RED: "pic/red.png",
+    BlockType.GREEN: "pic/green.png",
+    BlockType.BLUE: "pic/blue.png",
+    BlockType.YELLOW: "pic/yellow.png",
+    BlockType.ORANGE: "pic/orange.png",
+    BlockType.PURPLE: "pic/purple.png",
+    BlockType.CYAN: "pic/cyan.png",
+}
+
 class Block(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, blockType, pos):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('pic/red.png')
+        self.image = pygame.image.load(BLOCK_RES[blockType])
         self.rect = self.image.get_rect()
-        self.rect.center = (400, 300)
+        self.rect.center = pos
+
     def update(self):
         pressed = pygame.key.get_pressed()
         if pressed[K_LEFT]:
-            self.rect.move_ip(-1 , 0)
+            self.rect.move_ip(-1, 0)
         elif pressed[K_RIGHT]:
-            self.rect.move_ip(1 , 0)
+            self.rect.move_ip(1, 0)
         elif pressed[K_UP]:
-            self.rect.move_ip(0 , -1)
+            self.rect.move_ip(0, -1)
         elif pressed[K_DOWN]:
-            self.rect.move_ip(0 , 1)
+            self.rect.move_ip(0, 1)
+
     def draw(self, surface):
         surface.blit(self.image, self.rect)
-B = Block()
+
+P = Block(BlockType.PURPLE, (200, 300))
+C = Block(BlockType.CYAN, (600, 300))
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    B.update()
-    DIsplay.fill( (0,0,0) ) 
-    B.draw(DIsplay)
+    P.update()
+    C.update()
+    DISPLAY.fill((0, 0, 0))
+    P.draw(DISPLAY)
+    C.draw(DISPLAY)
+    pygame.display.update()
+import pygame
+import sys
+from pygame.locals import *
+import random
+
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+DISPLAY = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+class BlockType:
+    RED = 0
+    GREEN = 1
+    BLUE = 2
+    YELLOW = 3
+    ORANGE = 4
+    PURPLE = 5
+    CYAN = 6
+    BLOCKMAX = 7
+
+BLOCK_RES = {
+    BlockType.RED: "pic/red.png",
+    BlockType.GREEN: "pic/green.png",
+    BlockType.BLUE: "pic/blue.png",
+    BlockType.YELLOW: "pic/yellow.png",
+    BlockType.ORANGE: "pic/orange.png",
+    BlockType.PURPLE: "pic/purple.png",
+    BlockType.CYAN: "pic/cyan.png",
+}
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, blockType, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(BLOCK_RES[blockType])
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+    def update(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        elif pressed[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+        elif pressed[K_UP]:
+            self.rect.move_ip(0, -1)
+        elif pressed[K_DOWN]:
+            self.rect.move_ip(0, 1)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+P = Block(BlockType.PURPLE, (200, 300))
+C = Block(BlockType.CYAN, (600, 300))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    P.update()
+    C.update()
+    DISPLAY.fill((0, 0, 0))
+    P.draw(DISPLAY)
+    C.draw(DISPLAY)
     pygame.display.update()
