0422d46a70d209c146ada35688714895c6c5268f
hi <138982320+hello-world-html@users.noreply.github.com>
2023-10-06 13:57:07 +0800

    5

--- game/test/pygame.py
+++ game/test/pygame.py
@@ -73,26 +73,6 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 DISPLAY = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-class BlockType:
-    RED = 0
-    GREEN = 1
-    BLUE = 2
-    YELLOW = 3
-    ORANGE = 4
-    PURPLE = 5
-    CYAN = 6
-    BLOCKMAX = 7
-
-BLOCK_RES = {
-    BlockType.RED: "pic/red.png",
-    BlockType.GREEN: "pic/green.png",
-    BlockType.BLUE: "pic/blue.png",
-    BlockType.YELLOW: "pic/yellow.png",
-    BlockType.ORANGE: "pic/orange.png",
-    BlockType.PURPLE: "pic/purple.png",
-    BlockType.CYAN: "pic/cyan.png",
-}
-
 class Block(pygame.sprite.Sprite):
     def __init__(self, blockType, pos):
         pygame.sprite.Sprite.__init__(self)
